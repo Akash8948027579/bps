a22a8af3a748bbfa8665dd86f03cc1c19aae06c4
Akash8948027579 <akashkushwaha0771@gmail.com>
2024-01-04 18:18:13 +0530

    hello

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css';
+import Bpsbooking from './Components'
+
+function App() {
+  return (
+    <>
+      <Bpsbooking/>
+    </>
+  );
+}
+
+export default App;
